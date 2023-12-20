7451e79d7f6d28640153b29e6c937934c6b9014b
Owen Ritzau <oritzau2021@gmail.com>
2023-12-20 03:45:04 -0500

    Added puzzle to dad's, got more images for meme

--- script.js
+++ script.js
@@ -3,40 +3,40 @@ document.getElementById("codeForm").addEventListener("submit", function (event) 
 
     const enteredCode = document.getElementById("codeInput").value;
 
-    // TODO: add actual codes
-    const TEST = "0000";
-    const KATE = "BigKittyQuishMe";
-    const MEG = "MilanoCookie"
-    const MOM = "SheIsAbroad"
-    const DAD = "BlooberTurtle"
-    const GRANDAD = "ButtonMan";
-    const MEGHAN = "Bubby<3"
-    const MEME = "IdkChangeThisLater"
+    const TEST      = "0000";
+    const KATE      = "BigKittyQuishMe";
+    const MEG       = "MilanoCookie";
+    const MOM       = "SheIsAbroad";
+    const DAD       = "BlooberTurtle";
+    const GRANDAD   = "ButtonMan";
+    const MEGHAN    = "Bubby<3";
+    const MEME      = "IdkChangeThisLater";
 
     switch (enteredCode) {
         case TEST:
-            window.location.href = "/chrimmas/test.html"
-            break
-        case KATE:
-            window.location.href = "/chrimmas/kate.html"
+            window.location.href = "/chrimmas/test.html";
+            break;
+        case KATE: // done?
+            window.location.href = "/chrimmas/kate.html";
             break;
         case MEG:
-            window.location.href = "/chrimmas/meg.html"
+            window.location.href = "/chrimmas/meg.html";
             break;
-        case MOM:
-            window.location.href = "/chrimmas/mom.html"
+        case MOM: // done
+            window.location.href = "/chrimmas/mom.html";
             break;
-        case DAD:
-            window.location.href = "/chrimmas/dad.html"
+        case DAD: // TODO: add 3rd puzzle
+            window.location.href = "/chrimmas/dad.html";
             break;
         case GRANDAD:
-            window.location.href = "/chrimmas/grandad.html"
+            window.location.href = "/chrimmas/grandad.html";
             break;
-        case MEGHAN:
-            window.location.href = "/chrimmas/meghan.html"
+        case MEGHAN: // done
+            window.location.href = "/chrimmas/meghan.html";
             break;
-        case MEME:
-            window.location.href = "/chrimmas/meme.html"
+        case MEME: // TODO: fix code and maybe add some stuff?
+            window.location.href = "/chrimmas/meme.html";
+            break;
         default:
             break;
     }
